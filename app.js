8242ec42a2be74953dca0934f8a9058056d8e414
Glitch (alasnaobot) <none>
2021-09-24 09:55:58 +0000

    Depuis glitch

---
+++ app.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// Imports dependencies and set up http server
+const request = require("request"),
+  express = require("express"),
+  body_parser = require("body-parser"),
+  app = express().use(body_parser.json()); // creates express http server
+
+const venom = require("venom-bot");
+
+venom
+  .create()
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+function start(client) {
+  client.onMessage((message) => {
+    if (message.body === 'Hi' && message.isGroupMsg === false) {
+      client
+        .sendText(message.from, 'Welcome Venom 🕷')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    }
+  });
+}
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
+
+app.get("/", (req, res) => {
+  res.status(200).send("Bienvenue sur Maxofy");
+});
